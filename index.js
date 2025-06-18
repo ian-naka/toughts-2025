029bdff0ec73a81870477c84b9e58c0416aa0e84
Ian Nakamura <163802766688@estudante.ufjf.br>
2025-06-18 20:58:58 -0300

    fix

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const session = require('express-session')
+const session = require('express-session') //o session é responsável por guardar dados do usuário entre as requisições
 const flash = require('express-flash')
 const FileStore = require('session-file-store')(session)
 
 
 const app = express() // inicializacao do express
-
 const conn = require('./db/conn')
 
 const Tought = require('./models/Tought')
@@ -25,20 +24,20 @@ app.use(express.json())
 
 //session middleware
 app.use(
-    session({
-        name: 'session',
-        secret: 'nosso_secret',
-        resave: false,
-        saveUninitialized: false,
-        store: new FileStore({
+    session({ //implementa que todas as requisições passam por esse middleware de sessão
+        name: 'session', //nome do cookie
+        secret: 'nosso_secret', //chave para acessar o cookie
+        resave: false, // ñ vai salvar a sessão de novo no servidor a cada requisição
+        saveUninitialized: false, //só cria a sessão se o usuário colocar informação de valor
+        store: new FileStore({ // armazena as sessões em arquivos locais
             logFn: function () {},
             path: require('path').join(require('os').tmpdir(), 'sessions'),
         }),
         cookie: {
-            secure: false,
-            maxAge: 360000,
+            secure: false, //permite que o cookie funcione em http
+            maxAge: 360000, //cookie expira em 6 minutos
             expires: new Date(Date.now() + 360000),
-            httpOnly: true,
+            httpOnly: true, //o cookie nao pode ser acessado via navegador, so por servidor
         },
     })
 );
