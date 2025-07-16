00f915f2f7dcd5f072542553cd97b03dd0b6c80a
Ian Nakamura <163802766688@estudante.ufjf.br>
2025-07-16 18:25:37 -0300

    Registrando usuário no sistema pt1

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,14 +8,57 @@ module.exports = class authController{
     static register(req, res){
         res.render('auth/register')
     }
-    static async registerPost (req, res){
-        const {name, email, password, confirmpassword } = req.body
-        //validação de senha
-        if(password != confirmpassword){
-            req.flash('message', 'As senhas não conferem, tente novamente!')
-            res.render('auth/register')
+    
+static async registerPost(req, res) {
 
-            return
-        }
+    const {name, email, password, confirmpassword } = req.body
+
+    //password match validation
+
+    if(password != confirmpassword) {
+        //mensagem
+        req.flash('message', 'As senhas não conferem, tente novamente!')
+        res.render('auth/register')
+
+        return
     }
+
+
+    const checkifUserExists = await User.findOne({where: {email: email}})
+
+    if(checkifUserExists){
+        req.flash('message', 'O e-mail já está em uso!')
+        res.render('auth/register')
+        return
+    }
+
+    //create a password
+
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(password, salt)
+
+
+    const user = {
+        name,
+        email,
+        password: hashedPassword
+    }
+
+    try {
+        await User.create(user)
+        const createdUser = await User.create(user)
+
+        // initialize session
+        req.session.userid = createdUser.id
+
+
+        req.flash('message', 'Cadastro realizado com sucesso!')
+        req.session.save(() => {
+            res.redirect('/')
+        })
+        
+    } catch(err){
+        console.log(err)
+    }
+}
 }
