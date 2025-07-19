4bd594cfa1616e85c06c2b6ce88f9bd03d7bbc45
Ian Nakamura <163802766688@estudante.ufjf.br>
2025-07-19 13:11:03 -0300

    Middleware de autenticação

--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router()
 // controller
 
 const ToughtController = require('../controllers/ToughtController')
+const checkAuth = require('../helpers/auth').checkAuth
+
 router.get('/', ToughtController.showToughts)
+router.get('/dashboard', checkAuth, ToughtController.dashboard)
 
 module.exports = router
