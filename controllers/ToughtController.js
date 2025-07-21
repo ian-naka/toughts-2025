9663dfca8422181ffa30030cde2674fdd424bd6f
Ian Nakamura <163802766688@estudante.ufjf.br>
2025-07-21 16:22:26 -0300

    Resgatando pensamentos para a Dashboard

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -6,7 +6,17 @@ module.exports = class ToughtsController{
         res.render('toughts/home')
     }
     static async dashboard(req,res){
-        res.render('toughts/dashboard')
+        const userId = req.session.userid
+        const user = await User.findOne({where:{id: userId},
+        include: Tought,
+        plain: true,
+        })
+
+        if(!user){
+            res.redirect('/login')
+        }
+        const toughts = user.Toughts.map((result) => result.dataValues)
+        res.render('toughts/dashboard', { toughts })
     }
     static createTought(req,res){
         res.render('toughts/create')
