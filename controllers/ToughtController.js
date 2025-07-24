cd355db58b8657c2c8de635f5265017f586dabb2
Ian Nakamura <163802766688@estudante.ufjf.br>
2025-07-24 18:18:47 -0300

    Resgatando pensamentos para a home

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -3,7 +3,12 @@ const User = require('../models/User')
 
 module.exports = class ToughtsController{
     static async showToughts(req, res){
-        res.render('toughts/home')
+
+        const toughtsData = await Tought.findAll({
+            include: User,
+        })
+        const toughts = toughtsData.map((result) => result.get({plain: true}))
+        res.render('toughts/home', { toughts})
     }
     static async dashboard(req,res){
         const userId = req.session.userid
